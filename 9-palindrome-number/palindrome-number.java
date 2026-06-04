332b8bc796369ffdd7d3e6c6810f928f9c070641
Varsha-Lavania <the.varsha.lavania@gmail.com>
2026-06-04 22:50:45 +0530

    Time: 8 ms (9.80%) | Memory: 46 MB (33.39%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean isPalindrome(int x) {
 
-        String ns = "";
         String s = String.valueOf(x);
 
-        for(int i = s.length() - 1; i >= 0; i--) {
-            ns = ns + s.charAt(i);
-        }
+        String rev = new StringBuilder(s).reverse().toString();
 
-        return s.equals(ns);
+        return s.equals(rev);
     }
 }
